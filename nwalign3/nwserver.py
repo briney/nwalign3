034f218d5075b5d7c896806c39381ba920cd5eca
bryanbriney <bryanbriney@Bryans-MacBook-Pro.local>
2017-10-20 22:46:59 -0700

    fix print statements for Py3 compatibility

--- nwalign3/nwserver.py
+++ nwalign3/nwserver.py
@@ -1,4 +1,7 @@
+from __future__ import print_function
+
 import sys
+
 
 def main(port=1233):
     import socket
@@ -7,12 +10,12 @@ def main(port=1233):
 
     PORT = int(port)
     CHUNK = 32768 * 8
-    HOST = 'localhost' 
+    HOST = 'localhost'
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((HOST, PORT))
     server.listen(4)
-    print "\nstarted server on %s:%i\n" % (HOST, PORT)
+    print("\nstarted server on %s:%i\n" % (HOST, PORT))
 
     def get_args(astr):
         kwargsab = astr.split(" ")
@@ -29,17 +32,17 @@ def main(port=1233):
 
     while True:
         client, address = server.accept()
-        data = True 
+        data = True
         while data:
-            try: 
+            try:
                 data = client.recv(CHUNK).strip()
                 if data == "EXIT":
                     client.close()
                     server.close()
-                    print "EXITING service"
+                    print("EXITING service")
                     sys.exit(0)
 
-                a,b, kwargs = get_args(data)
+                a, b, kwargs = get_args(data)
                 r = global_align(a, b, **kwargs)
                 client.send(" ".join(r))
             except Exception, e:

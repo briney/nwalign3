f048f9c659a085feba7cfce8b2090daad39bf9ef
bryanbriney <bryanbriney@Bryans-MacBook-Pro.local>
2017-10-23 16:29:02 -0700

    update global_align_wrapper to be compatible with cli usage

--- nwalign3/wrappers.py
+++ nwalign3/wrappers.py
@@ -6,11 +6,14 @@ import sys
 from .cnwalign import global_align, global_align_no_matrix, score_alignment
 
 
-def global_align_wrapper(s1, s2, **kwargs):
+def global_align_wrapper(s1, s2, matrix=None, match=1, gap_open=-1, gap_extend=-1, server=0):
+    match = int(match)
+    gap_open = int(gap_open)
+    gap_extend = int(gap_extend)
     if sys.version_info[0] >= 3:
         s1 = s1.encode()
         s2 = s2.encode()
-    a1, a2 = global_align(s1, s2, **kwargs)
+    a1, a2 = global_align(s1, s2, match, gap_open, gap_extend, matrix)
     return (a1.decode(), a2.decode())
 
 
